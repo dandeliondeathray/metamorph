0a442432864d8885d62fc7d815732b60458b3881
Erik Edin <erikedin@users.noreply.github.com>
2017-10-01 22:04:31 +0200

    add actual kafka producer to the test suite

--- features/environment.py
+++ features/environment.py
@@ -1,9 +1,14 @@
 import pymetamorph.pymetamorph.metamorph as morph
-
+from confluent_kafka import Producer
 
 class Service:
+    def __init__(self):
+        self._producer = Producer({'bootstrap.servers': 'localhost:9092'})
+
     def send_message(self, key, value, topic):
         print("Sending {}, {} to topic {}".format(key, value, topic))
+        self._producer.produce(topic, value=value, key=key)
+        self._producer.flush()
 
 
 def before_feature(context, feature):
