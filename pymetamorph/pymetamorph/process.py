2dccd529b14e3a0b7ffaf5498b1bacb50d1c8c8f
Erik Edin <erikedin@users.noreply.github.com>
2017-11-03 13:36:55 +0100

    redirect stdout and stderr to file

--- pymetamorph/pymetamorph/process.py
+++ pymetamorph/pymetamorph/process.py
@@ -9,11 +9,26 @@ import tempfile
 
 
 class Process:
-    def __init__(self, popen):
+    def __init__(self, popen, out, err):
         self._popen = popen
+        self._out = out
+        self._err = err
 
     def stop(self):
         self._popen.terminate()
+
+    def out_name(self):
+        return self._out.name
+
+    def err_name(self):
+        return self._err.name
+
+
+def ensure_exists(path):
+    try:
+        os.makedirs(path)
+    except OSError:
+        pass
 
 
 def start(go=None, args="", env=None):
@@ -22,6 +37,10 @@ def start(go=None, args="", env=None):
     except KeyError:
         gopath = os.path.join(os.environ['HOME'], 'go')
 
+    ensure_exists('./behave_output')
+    out = tempfile.NamedTemporaryFile(prefix=os.path.basename(go) + "_test_out", delete=False, dir='./behave_output')
+    err = tempfile.NamedTemporaryFile(prefix=os.path.basename(go) + "_test_err", delete=False, dir='./behave_output')
+
     process_path = os.path.join(gopath, go)
-    popen_obj = subprocess.Popen([process_path] + args.split(), env=env)
-    return Process(popen_obj)
+    popen_obj = subprocess.Popen([process_path] + args.split(), stdout=out, stderr=err, env=env)
+    return Process(popen_obj, out, err)
